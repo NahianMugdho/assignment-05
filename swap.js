cc44a612769dfff30d3cda4570051983616048cf
NahianMugdho <nahianmugdho@gmail.com>
2024-09-25 21:23:19 +0600

    color-change

--- swap.js
+++ swap.js
@@ -1,8 +1,12 @@
 document.getElementById('bt-2').addEventListener('click',function(){
-
+    
     getclass('history').remove('hidden')
     
-    getclass('cards').add('hidden')
+    getclass('cards').add('hidden')   
+    document.getElementById('bt-2').classList.remove('bg-slate-300');
+    document.getElementById('bt-2').classList.add('bg-[#B4F461]');
+    document.getElementById('bt-1').classList.remove('bg-[#B4F461]');
+    document.getElementById('bt-1').classList.add(' bg-slate-300 ');
     
     })
     
@@ -12,6 +16,9 @@ document.getElementById('bt-2').addEventListener('click',function(){
     
         getclass('cards').remove('hidden')
 
-      
+       getclass('bt-1').remove('bg-slate-300');
+        document.getElementById('bt-1').classList.add('bg-[#B4F461]');
+        document.getElementById('bt-2').classList.remove('bg-[#B4F461]');
+        document.getElementById('bt-2').classList.add(' bg-slate-300 ');
         
         })
